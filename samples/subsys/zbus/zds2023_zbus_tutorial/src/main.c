96e380e2abf9bdb499483ae4f9721d39128c2ab1
Rodrigo Peixoto <rodrigopex@gmail.com>
2023-06-24 17:11:11 -0300

    Tutorial step 06
    Add the timer plus work queue to start triggering the sensor thread.
    
    Signed-off-by: Rodrigo Peixoto <rodrigopex@gmail.com>

--- samples/subsys/zbus/zds2023_zbus_tutorial/src/main.c
+++ samples/subsys/zbus/zds2023_zbus_tutorial/src/main.c
@@ -34,6 +34,20 @@ ZBUS_CHAN_DEFINE(transmission_done_chan, /* Channel name */
 		 ZBUS_OBSERVERS_EMPTY,   /* Observers */
 		 false);
 
+struct k_work start_trigger_work;
+
+void execute_start_trigger(struct k_work *work)
+{
+	zbus_chan_notify(&start_trigger_chan, K_MSEC(250));
+}
+
+void trigger_timer_handler(struct k_timer *dummy)
+{
+	k_work_submit(&start_trigger_work);
+}
+
+K_TIMER_DEFINE(trigger_timer, trigger_timer_handler, NULL);
+
 int main(void)
 {
 	struct project_info_msg *prj_info =
@@ -53,11 +67,15 @@ int main(void)
 	zbus_chan_add_obs(&payload_chan, &mock_lis, K_NO_WAIT);
 	zbus_chan_add_obs(&transmission_done_chan, &mock_lis, K_NO_WAIT);
 
-	while (1) {
-		zbus_chan_notify(&start_trigger_chan, K_FOREVER);
+	/* while (1) { */
+	/* 	zbus_chan_notify(&start_trigger_chan, K_FOREVER); */
+	/**/
+	/* 	k_msleep(2000); */
+	/* } */
 
-		k_msleep(2000);
-	}
+	k_work_init(&start_trigger_work, execute_start_trigger);
+
+	k_timer_start(&trigger_timer, K_SECONDS(2), K_SECONDS(2));
 
 	return 0;
 }
